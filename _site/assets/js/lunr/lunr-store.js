7e6a13afc4cffa2a56dd770ff4459ccf97e5cbf4
William Robinet <william.robinet@conostix.com>
2025-05-14 16:06:34 +0200

    chg: [blog post] BSidesLuxembourg 2025

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,10 +2,15 @@ var store = [{
         "title": "hack.lu 2024 Wraps Up a Thrilling 18th Edition with Global Participation and Networking Highlights",
         "excerpt":"Luxembourg, November 04, 2024 – hack.lu, Europe’s central hub for cybersecurity professionals, enthusiasts, and experts, celebrated its 18th successful edition from October 22-25 in Luxembourg, hosting a packed agenda of workshops, trainings, and talks. With over 500 participants from around the world and 75 expert speakers, this year’s conference once...","categories": ["blog"],
         "tags": [],
-        "url": "/blog/hack.lu-2024-wraps-up/",
+        "url": "http://localhost:4000/blog/hack.lu-2024-wraps-up/",
         "teaser":null},{
         "title": "Call for papers is now open for hack.lu 2025",
         "excerpt":"hack.lu 2025 Call for Papers (including cti-summit) The purpose of the hack.lu convention is to provide an open and free playground where people can discuss the implications of new technologies in society. hack.lu is a balanced mix convention where technical and non-technical people can meet and share all kinds of...","categories": ["blog"],
         "tags": [],
-        "url": "/blog/hack.lu-2025-call-for-papers/",
+        "url": "http://localhost:4000/blog/hack.lu-2025-call-for-papers/",
+        "teaser":null},{
+        "title": "BSidesLuxembourg is back in 2025, June 19th in Belval",
+        "excerpt":"BSidesLuxembourg is back in 2025! Calling all hackers in and around Luxembourg!As many of you know, BSidesLuxembourg ran three times from 2017-2019 and thenCOVID hit to our collective dismay and abruptly many events, hack.lu included,stopped for a number of years. Well, we’re now bringing BSidesLuxembourg back, and we’re planning to...","categories": ["blog"],
+        "tags": [],
+        "url": "http://localhost:4000/blog/hack.lu-2025-bsides.lu/",
         "teaser":null}]
